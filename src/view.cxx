96f19c1535543de4f5ef6aeefc65764b384b5078
elliottyoon <elinate91@gmail.com>
2022-03-06 16:36:58 -0600

    made crosshair position a instance field

--- src/view.cxx
+++ src/view.cxx
@@ -7,19 +7,17 @@ View::View(Model const& model)
         , ball_sprite_{5, Color::white()} // TODO: radius should be variable
         , crosshair_sprite_("crosshair.png")
         , crosshair_dims_{50, 50}
-        , crosshair_posn_{(int) ((model_.get_model_dims().width /
-                                  2) - (crosshair_dims_.width / 2)),
-                          (int) (
-                                  (model_
-                                           .get_model_dims()
-                                           .height / 2) - (crosshair_dims_.height
-                                                           / 2))}
+        , crosshair_posn_{(int) ((model_.get_model_dims().width / 2)
+                                    - (crosshair_dims_.width / 2)),
+                          (int) ((model_.get_model_dims().height / 2)
+                                    - (crosshair_dims_.height/ 2))}
+                                    // TODO: this is jank asf
 { }
 
 void
 View::draw(ge211::Sprite_set& set)
 {
-    // draws crossshair. z-value: 10 TODO: probably gonna want to change this
+    // draws crossshair. z-value: 10
     set.add_sprite(crosshair_sprite_, crosshair_posn_, 10);
 
     // draws each ball in the balls vector. z-value: 1
